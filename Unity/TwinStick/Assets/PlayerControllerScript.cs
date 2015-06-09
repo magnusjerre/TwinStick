6b31325ceeb16c95a9831f51b108d6ceabc28e87
Magnus Jerre <magnus.jerre@gmail.com>
2015-06-09 11:18:02 +0200

    Added simple walk blend animation

---
+++ Unity/TwinStick/Assets/PlayerControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+	Animator anim;
+	Rigidbody body;
+	public float speed = 5;
+
+	void Awake() {
+		anim = GetComponent<Animator> ();
+		body = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		if (Input.GetAxis ("Vertical") > 0.1) {
+			body.velocity = Vector3.forward * speed;
+			anim.SetFloat ("speed", 0.21f);
+		} else if (Input.GetAxis ("Vertical") < -0.1) {
+			body.velocity = Vector3.back * speed;
+			anim.SetFloat("speed", 0.2f);
+		} else {
+			body.velocity = Vector3.zero;
+			anim.SetFloat ("speed", 0.0f);
+		}
+	}
+
+}
